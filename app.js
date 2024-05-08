5c66958b9f558c553c9346948a68c695683a2a9f
prabhavsetia <prabhav.setia21@gmail.com>
2024-05-08 13:06:45 +0530

    StepUp Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 8000;
+const fs = require("fs");
+
+//EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static'))//for serving static files
+app.use(express.urlencoded())
+
+//PUG SPECIFIC STUFF
+app.set('view engine', 'pug')// set the template engine as pug 
+app.set('views',path.join(__dirname,'views'))// set the views directory
+
+//END POINTS
+app.get('/',(req, res)=>{
+    const params = {}
+    res.status(200).render('index.pug',params);
+})
+
+// START THE SERVER
+app.listen(port, ()=>{
+    console.log(`This applicatin will be working on port ${port}`);
+})
